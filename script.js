33d9c37cdc29ec755f4f4b85319b26a0338a4466
technova <ap25india@gmail.com>
2025-01-27 12:24:01 +0530

    add more new file

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let fd=document.getElementById('fd')
+let recieps = document.getElementById('recieps')
+let home = document.getElementById('home')
 
-fd.addEventListener('click',()=>{
-     window.location="pro.html"
+
+let mr = document.getElementById('mr')
+
+recieps.addEventListener('click', () => {
+     window.location = "pro.html"
+})
+
+
+
+home.addEventListener('click', () => {
+     window.location = "index.html"
+})
+
+mr.addEventListener('click', () => {
+     window.location = "pro.html"
 })
